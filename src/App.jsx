0afa19433538e1e032892d8b2758fcaf78612e85
Dorian Buck <[dorianbuck@gmail.com]>
2021-09-06 14:01:18 +0200

    displays hello world from class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return <div>
+      <h1>Hello World from a class Component</h1>
+    </div>;
+  }
 }
 
-export default App
+export default App;
